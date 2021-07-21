b9b5322af3a9ba849fe9bdb6513465d7057fa467
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-07-21 14:33:09 -0300

    feat: new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoInputComponent } from './todo/components/todo-input/todo-input.component';
+import { TodoFilterComponent } from './todo/components/todo-filter/todo-filter.component';
+import { TodoListComponent } from './todo/components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoInputComponent,
+    TodoFilterComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule
