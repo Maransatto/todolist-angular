c46e591f781bb120038ee113fa54308ee7001336
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-07-21 15:56:49 -0300

    feat(todo-list.model.ts): improved the attributes and creted a enum

--- src/app/todo/models/todo-list.model.ts
+++ src/app/todo/models/todo-list.model.ts
@@ -3,10 +3,21 @@ import { TodoItem } from "./todo-item.model";
 export class TodoList {
     filter: string;
     status: number;
-    todoList: TodoItem[] = [];
+    items: TodoItem[];
 
-    constructor(filter: string, status: number) {
+    constructor(
+        filter: string = '',
+        status: number = TodoStatus.toBeDone,
+        items: TodoItem[] = []
+    ) {
         this.filter = filter;
         this.status = status;
+        this.items = items
     }
+}
+
+export enum TodoStatus {
+    toBeDone = 1,
+    completed = 2,
+    all = 3
 }
