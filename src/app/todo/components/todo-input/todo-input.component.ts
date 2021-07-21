f6a729eec71d26759cf0dd6b3fc81c1f298f19af
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-07-21 17:12:26 -0300

    feat(todo-input.component): calling the newTask method from facade

--- src/app/todo/components/todo-input/todo-input.component.ts
+++ src/app/todo/components/todo-input/todo-input.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TodoFacade } from '../../todo.facade';
 
 @Component({
   selector: 'app-todo-input',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private todoFacade: TodoFacade
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form = this.formBuilder.group({
+      description: [null, Validators.required]
+    })
+  }
+
+  submitNewTask() {
+    if (!this.form.valid) {
+      return;
+    }
+
+    const { description } = this.form.value;
+    this.todoFacade.addNewTask(description);
   }
 
 }
