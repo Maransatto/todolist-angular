45c1ca6853f557272f1c8dde22c510282574e00e
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-07-21 17:08:28 -0300

    feat(todo.service.ts): postNewTask
    The idea is to simulate a situation that sometimes gets an error from
    the api

--- src/app/todo/api/todo.service.ts
+++ src/app/todo/api/todo.service.ts
@@ -7,7 +7,7 @@ import { TodoItem } from "../models/todo-item.model";
 export class todoService {
     
     getTodoList(): Promise<TodoItem[]> {
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             // supposing this is a return of a rest api
             const theList = [
                 new TodoItem('Go to shopping', false),
@@ -17,6 +17,20 @@ export class todoService {
             setTimeout(() => {
                 resolve(theList);
             }, 1000);
+        })
+    }
+
+    postNewTask(item: TodoItem): Promise<any> {
+        return new Promise((resolve, reject) => {
+            const itWorked = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+            setTimeout(() => {
+                if (itWorked) {
+                    resolve(true);
+                } else {
+                    reject(false);
+                }
+            }, 500);
+
         })
     }
 
