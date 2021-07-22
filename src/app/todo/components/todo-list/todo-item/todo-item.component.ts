f054f64af0fc683e2507462e23f127609bb6887a
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-07-22 09:33:27 -0300

    feat(todo-item): change layout if item is completed
    added ngIf to show the proper buttons

--- src/app/todo/components/todo-list/todo-item/todo-item.component.ts
+++ src/app/todo/components/todo-list/todo-item/todo-item.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TodoItem } from 'src/app/todo/models/todo-item.model';
 
 @Component({
-  selector: '[app-todo-item]',
+  selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
