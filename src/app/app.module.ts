84380e92a1c0628caf770f2123eac9f5bfcc2092
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-07-22 09:15:25 -0300

    refactor(todo-item): new component to hold the item on the list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { TodoInputComponent } from './todo/components/todo-input/todo-input.comp
 import { TodoFilterComponent } from './todo/components/todo-filter/todo-filter.component';
 import { TodoListComponent } from './todo/components/todo-list/todo-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TodoItemComponent } from './todo/components/todo-list/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoInputComponent,
     TodoFilterComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/todo/components/todo-list/todo-item/todo-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TodoItem } from 'src/app/todo/models/todo-item.model';
+
+@Component({
+  selector: '[app-todo-item]',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() item!: TodoItem;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
