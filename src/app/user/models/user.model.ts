cd69619f5a11b0f26f66a90860fc2c3b738c987e
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-07-23 00:07:26 -0300

    feat(user.model.ts): user model with attributes

---
+++ src/app/user/models/user.model.ts
@@ -0,0 +1,14 @@
+export class User {
+    
+    id: number;
+    name: string;
+    email!: string;
+    password!: string;
+    
+    constructor(id: number, name: string, email: string, password: string) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
