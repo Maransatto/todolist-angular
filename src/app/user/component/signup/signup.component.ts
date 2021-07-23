83a445e70ba6d4de5d68601cb895e2775dcd5ff7
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-07-23 00:09:07 -0300

    feat(signup.component): signup basic feature

--- src/app/user/component/signup/signup.component.ts
+++ src/app/user/component/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserFacade } from '../../user.facade';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userFacade: UserFacade
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form = this.formBuilder.group({
+      name: [null, Validators.required],
+      email: [null, Validators.required],
+      password: [null, Validators.required],
+      checkPassword: [null, Validators.required]
+    });
+  }
+
+  signup() {
+    const { name, email, password, checkPassword } = this.form.value;
+    this.userFacade.addNewUser(name, email, password, checkPassword);
   }
 
 }
