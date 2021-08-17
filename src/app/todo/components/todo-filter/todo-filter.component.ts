bd56e9db8eaa49aa0c9aee898fb7a22a4abcbbc1
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-08-17 09:10:35 -0300

    feat(todo-filter): using the selector to apply filter

--- src/app/todo/components/todo-filter/todo-filter.component.ts
+++ src/app/todo/components/todo-filter/todo-filter.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { TodoStatus } from '../../models/todo-filter';
-import { TodoFacade } from '../../todo.facade';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { TodoFilter, TodoStatus } from '../../models/todo-filter';
+// import { TodoFacade } from '../../todo.facade';
+
+import * as fromApp from 'src/app/state/app.state';
+import * as TodoActions from '../../../state/todo.actions'
 
 @Component({
   selector: 'app-todo-filter',
@@ -11,27 +16,31 @@ import { TodoFacade } from '../../todo.facade';
 export class TodoFilterComponent implements OnInit {
 
   form!: FormGroup;
+  subscription!: Subscription;
 
   constructor(
     private formBuilder: FormBuilder,
-    private todoFacade: TodoFacade
+    // private todoFacade: TodoFacade,
+    private store: Store<fromApp.AppState>
   ) { }
 
   ngOnInit(): void {
-      this.createForm();
-      this.submitFilter();
+    this.subscription = this.store.select('filter').subscribe(filterState => {
+      this.createForm(filterState.filter.description, <TodoStatus>filterState.filter.status);
+    });
   }
 
-  createForm(): void {
+  createForm(description: string, status: TodoStatus): void {
     this.form = this.formBuilder.group({
-      description: [null],
-      status: [TodoStatus.toBeDone]
+      description: [description],
+      status: [status]
     })
   }
 
   submitFilter() {
     const { description, status } = this.form.value;
-    this.todoFacade.changeFilter(description, status);
+    // this.todoFacade.changeFilter(description, status);
+    this.store.dispatch(TodoActions.setFilter({ filter: new TodoFilter(description, status) }));
   }
 
 }
