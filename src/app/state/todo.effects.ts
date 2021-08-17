f5d7b8f55ddbecef4b105c00a20257be5dade496
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-08-17 11:48:23 -0300

    feat(todo.effects.ts): ngxService on the pessimistic effect

--- src/app/state/todo.effects.ts
+++ src/app/state/todo.effects.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 import { createEffect, Actions, ofType } from "@ngrx/effects";
 import { ToastrService } from "ngx-toastr";
+import { NgxUiLoaderService } from "ngx-ui-loader";
 import { EMPTY, of } from "rxjs";
-import { catchError, map, switchMap } from "rxjs/operators";
+import { catchError, map, switchMap, tap } from "rxjs/operators";
 import { TodoService } from "../todo/api/todo.service";
 
 import * as TodoActions from './todo.actions';
@@ -44,14 +45,17 @@ export class TodoEffects {
     // pessimistic (once startDeletingTask DOES NOT affect the state)
     deleteTaskApi$ = createEffect(() => this.actions$.pipe(
         ofType(TodoActions.startDeletingTask),
+        tap(() => this.ngxService.start()),
         switchMap(actions => this.todoService.removeTask(actions.task)
             .pipe(
                 map(() => {
                     this.toastr.success(`Task Removed: ${actions.task.description}`, 'Success');
+                    this.ngxService.stop();
                     return TodoActions.deleteTask({ task: actions.task }); // persists (as it is pessimistic)
                 }),
                 catchError(() => {
                     this.toastr.warning('There was an error on trying to remove. Please try again', 'Oops');
+                    this.ngxService.stop();
                     return EMPTY;
                 })
             ))
@@ -60,6 +64,8 @@ export class TodoEffects {
     constructor(
         private actions$: Actions,
         private todoService: TodoService,
-        private toastr: ToastrService
+        private toastr: ToastrService,
+        private ngxService: NgxUiLoaderService
+
     ) { }
 }
