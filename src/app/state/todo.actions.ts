ad8e383c4ec5056c3107514d879be19c521f5c40
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-08-16 22:42:44 -0300

    refactor(todo.actions.ts): new folder and props to setFilter

--- src/app/state/todo.actions.ts
+++ src/app/state/todo.actions.ts
@@ -1,11 +1,12 @@
 import { createAction, props } from "@ngrx/store";
-import { Task } from '../models/task.model';
+import { Task } from '../todo/models/task.model';
+import { TodoFilter } from "../todo/models/todo-filter";
 
 export const setTasks = createAction('[Todo] Set Tasks', props<{ tasks: Task[] }>()); // changing state (reducer)
 export const fetchTasks = createAction('[Todo] Fetch Tasks'); // via effects
 export const addTask = createAction('[Todo] Add Task', props<{ task: Task }>()); // changing state (reducer)
 export const storeTask = createAction('[Todo] Store Task', props<{ task: Task }>()); // via effects
 export const deleteTask = createAction('[Todo] Delete Task', props<{ taskId: number }>()); // changing state (reducer) 
-export const setFilter = createAction('[Todo] Set Filter'); // changing state (reducer)
+export const setFilter = createAction('[Todo] Set Filter', props<{ filter: TodoFilter }>()); // changing state (reducer)
 
 // export const applyFilter = createAction('[Todo] Apply Filter'); // changing state (reducer) // might be not necessary
