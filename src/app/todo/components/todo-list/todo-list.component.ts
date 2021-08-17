c11daaf113bd628ddd0b2da8a82320e401956ec8
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-08-16 22:49:05 -0300

    refactor(todo-list.component): get the tasks via ngrx

--- src/app/todo/components/todo-list/todo-list.component.ts
+++ src/app/todo/components/todo-list/todo-list.component.ts
@@ -1,31 +1,39 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Task } from '../../models/task.model';
-import { TodoFacade } from '../../todo.facade';
+// import { TodoFacade } from '../../todo.facade';
+
+import * as fromApp from 'src/app/state/app.state';
+
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit, OnDestroy {
+export class TodoListComponent implements OnInit {
 
-  tasks!: Task[];
-  subscription = new Subscription();
+  tasks!: Observable<Task[]>;
+  // subscription = new Subscription();
 
   constructor(
-    private todoFacade: TodoFacade
+    // private todoFacade: TodoFacade,
+    private store: Store<fromApp.AppState>
   ) { }
 
   ngOnInit(): void {
-    this.subscription.add(
-      this.todoFacade.getFilteredTasks$().subscribe(tasks => this.tasks = tasks)
-    );
-    this.todoFacade.loadTodoList();
+    this.tasks = this.store.select('tasks').pipe(map(tasksState => tasksState.tasks));
+
+    // this.subscription.add(
+    //   this.todoFacade.getFilteredTasks$().subscribe(tasks => this.tasks = tasks)
+    // );
+    // this.todoFacade.loadTodoList();
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   this.subscription.unsubscribe();
+  // }
 
 }
