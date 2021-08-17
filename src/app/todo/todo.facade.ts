b7b6b8fc18e148b083a12b2629297a710ec17a4f
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-08-17 11:42:59 -0300

    style(todo.facade.ts): cleaning up unused methods

--- src/app/todo/todo.facade.ts
+++ src/app/todo/todo.facade.ts
@@ -31,47 +31,6 @@ export class TodoFacade {
         return this.todoState.getFilteredTasks$();
     }
 
-    // loadTodoList() {
-    //     this.todoState.setTodoFilter(new TodoFilter());
-    //     this.ngxService.start();
-    //     // this.todoService.getTasks()
-    //     //     .then(theList => {
-    //     //         this.todoState.addTasks(theList);
-    //     //     }).finally(() => {
-    //     //         this.ngxService.stop();
-    //     //     });
-    // }
-
-    // addNewTask(description: string) {
-
-    //     const newTask = new Task(0, description, false);
-    //     // being optimistic
-    //     this.todoState.addSingleTask(newTask);
-    //     this.todoService.postNewTask(newTask)
-    //         .then(() => {
-    //             this.toastr.success(`New Task Added: ${description}`, 'Success');
-    //         })
-    //         .catch(err => {
-    //             this.toastr.warning('There was an error on trying to add a new task. Please try again', 'Oops');
-    //             this.todoState.removeTask(newTask);
-    //         });
-    // }
-
-    removeTask(task: Task) {
-        
-        // being pessimistic
-        this.ngxService.start();
-        this.todoService.removeTask(task)
-            .then(() => {
-                this.todoState.removeTask(task);
-                this.toastr.success(`Task Removed: ${task.description}`, 'Success');
-            }).catch(err => {
-                this.toastr.warning('There was an error on trying to remove. Please try again', 'Oops');
-            }).finally(() => {
-                this.ngxService.stop();
-            })
-    }
-
     completeTask(task: Task, completed: boolean) {
         // being pessimistic
         this.todoService.completeTask(task)
@@ -80,14 +39,5 @@ export class TodoFacade {
             }).catch(err => {
                 this.toastr.warning('There was an error on trying to complete. Please try again', 'Oops');
             })
-    }
-
-    changeFilter(description: string, status: TodoStatus) {
-        this.todoState.setTodoFilter(new TodoFilter(description, status));
-        this.applyFilter();
-    }
-
-    applyFilter() {
-        this.todoState.filter();
     }
 }
